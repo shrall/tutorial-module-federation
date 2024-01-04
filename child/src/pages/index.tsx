a2a8b42ebcf30d264d118c6338742e0828d7e97c
shrall <shrallvierdo@gmail.com>
2024-01-04 15:18:08 +0700

    feat: configure button component props

--- child/src/pages/index.tsx
+++ child/src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "parento/Button";
 export default function Home() {
   return (
     <main className="w-screen h-screen flex items-center justify-center">
-      <Button variant="secondary">Test</Button>
+      <Button variant="destructive">Test</Button>
     </main>
   );
 }
--- main/src/pages/index.tsx
+++ main/src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="w-screen h-screen flex items-center justify-center">
-      <Button variant="secondary">Test</Button>
+      <Button variant="destructive">Test</Button>
     </main>
   );
 }
